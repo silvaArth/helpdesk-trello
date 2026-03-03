90efaa9ca3da11c103bd4142b2d70773d837b17a
arthurSilva <arthurinacio1502@gmail.com>
2026-03-03 15:34:00 -0300

    feat: implementa service para criacao de chamados com insert no banco

--- services/chamado_service.py
+++ services/chamado_service.py
@@ -1,17 +1,27 @@
+import sqlite3 as lite
 from datetime import datetime
 
 def criar_chamado(dados):
-    chamado = {
-        "nome": dados["nome"],
-        "setor": dados["setor"],
-        "problema": dados["problema"],
-        "descricao": dados["descricao"],
-        "urgencia": dados["urgencia"],
-        "status": "Aberto",
-        "data_chamado": datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-    }
+    con = lite.connect("database/database.db")
+    cursor = con.cursor()
 
-    print("Chamado Criado:")
-    print(chamado)
+    data_atual = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 
-    return chamado
+    cursor.execute("""
+        INSERT INTO chamados_ti 
+        (nome, setor, problema, descricao, urgencia, status, data_chamado)
+        VALUES (?,?,?,?,?,?,?)
+        """, (
+            dados["nome"],
+            dados["setor"],
+            dados["problema"],
+            dados["descricao"],
+            dados["urgencia"],
+            "Aberto",
+            data_atual
+        ))
+    
+    con.commit()
+    con.close()
+
+    print("Chamado criado com sucesso!")
